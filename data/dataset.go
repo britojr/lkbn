b866008caa96d0c29bfd8e99785900b93b7b4d56
Joao Brito <joaojr.89@gmail.com>
2017-10-28 20:42:51 -0200

    data: add dataset

--- data/dataset.go
+++ data/dataset.go
@@ -1,4 +1,46 @@
 package data
 
+import (
+	"bufio"
+
+	"github.com/britojr/lkbn/vars"
+	"github.com/britojr/utl/errchk"
+	"github.com/britojr/utl/ioutl"
+	"github.com/kniren/gota/dataframe"
+)
+
 // Dataset ..
-type Dataset struct{}
+type Dataset struct {
+	vs vars.VarList
+	df dataframe.DataFrame
+}
+
+// NewDataset return a new dataset type
+func NewDataset(fname string) (d *Dataset) {
+	f := ioutl.OpenFile(fname)
+	defer f.Close()
+	d = new(Dataset)
+	d.df = dataframe.ReadCSV(bufio.NewReader(f))
+	for id, name := range d.df.Names() {
+		nstate := int(d.df.Col(name).Max()) + 1
+		v := vars.New(id, nstate)
+		v.SetName(name)
+		d.vs = append(d.vs, v)
+	}
+	return
+}
+
+// IntMaps return a slice of intmaps of the dataset
+func (d *Dataset) IntMaps() []map[int]int {
+	r, c := d.df.Dims()
+	var err error
+	ms := make([]map[int]int, r)
+	for i := 0; i < r; i++ {
+		ms[i] = make(map[int]int)
+		for j := 0; j < c; j++ {
+			ms[i][j], err = d.df.Elem(i, j).Int()
+			errchk.Check(err, "dataset: invalid data")
+		}
+	}
+	return ms
+}
--- data/evidence.go
+++
@@ -1,9 +0,0 @@
-package data
-
-// Evidence ..
-type Evidence map[int]int
-
-// EvidenceSet ...
-type EvidenceSet interface {
-	Observations() []Evidence
-}

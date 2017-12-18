6e63c6a4d5e380f9ddd22827328e881e7c3cb344
Joao Brito <joaojr.89@gmail.com>
2017-12-18 16:50:42 -0200

    learner: add variables group split

--- learner/bridge.go
+++ learner/bridge.go
@@ -1,9 +1,12 @@
 package learner
 
 import (
+	"fmt"
+
 	"github.com/britojr/btbn/scr"
 	"github.com/britojr/lkbn/data"
 	"github.com/britojr/lkbn/model"
+	"github.com/britojr/lkbn/vars"
 )
 
 // BridgeSearch implements the bridge strategy
@@ -20,7 +23,14 @@ func NewCTBridgeSearch() Learner {
 // Search searches for a network structure
 func (s *BridgeSearch) Search() Solution {
 	ct := model.SampleUniform(s.vs, s.tw)
-	s.paramLearner.Run(ct, s.ds.IntMaps())
+	// s.paramLearner.Run(ct, s.ds.IntMaps())
+
+	fmt.Println("Grouping variables per mutual information")
+	gs := splitGroups(s.ds.Variables(), s.tw, s.mutInfo)
+	for i, g := range gs {
+		fmt.Printf("%v: %v\n", i, g)
+	}
+
 	return ct
 }
 
@@ -28,4 +38,59 @@ func (s *BridgeSearch) Search() Solution {
 func (s *BridgeSearch) SetDataset(ds *data.Dataset) {
 	s.common.SetDataset(ds)
 	s.mutInfo = scr.ComputeMutInfDF(ds.DataFrame())
+}
+
+func splitGroups(vs vars.VarList, k int, mutInfo *scr.MutInfo) (gs []vars.VarList) {
+	remain := vs.Copy()
+	for len(remain) != 0 {
+		g := highestPair(remain, mutInfo)
+		remain.Remove(g[0].ID())
+		remain.Remove(g[1].ID())
+		for i := 2; i < k; i++ {
+			if len(remain) == 0 {
+				break
+			}
+			x := highestToGroup(g, remain, mutInfo)
+			g.Add(x)
+			remain.Remove(x.ID())
+		}
+		if len(remain) == 1 {
+			g.Add(remain[0])
+			remain.Remove(remain[0].ID())
+		}
+		gs = append(gs, g)
+	}
+	return
+}
+
+// finds the highest scoring pair of variables
+func highestPair(vs vars.VarList, mutInfo *scr.MutInfo) vars.VarList {
+	maxMI := 0.0
+	var x, y *vars.Var
+	for i, v := range vs {
+		for _, w := range vs[i+1:] {
+			mi := mutInfo.Get(v.ID(), w.ID())
+			if mi > maxMI {
+				maxMI = mi
+				x, y = v, w
+			}
+		}
+	}
+	return []*vars.Var{x, y}
+}
+
+// finds the highest scoring vair with relation to another group of variables
+func highestToGroup(vs, ws vars.VarList, mutInfo *scr.MutInfo) *vars.Var {
+	maxMI := 0.0
+	var x *vars.Var
+	for _, v := range vs {
+		for _, w := range ws {
+			mi := mutInfo.Get(v.ID(), w.ID())
+			if mi > maxMI {
+				maxMI = mi
+				x = w
+			}
+		}
+	}
+	return x
 }

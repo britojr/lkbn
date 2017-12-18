31127995b4ce8845884f8713c65645daaf544eb7
Joao Brito <joaojr.89@gmail.com>
2017-12-18 15:30:32 -0200

    learner: start bridge strategy

---
+++ learner/bridge.go
@@ -0,0 +1,31 @@
+package learner
+
+import (
+	"github.com/britojr/btbn/scr"
+	"github.com/britojr/lkbn/data"
+	"github.com/britojr/lkbn/model"
+)
+
+// BridgeSearch implements the bridge strategy
+type BridgeSearch struct {
+	*common              // common variables and methods
+	mutInfo *scr.MutInfo // pre-computed mutual information matrix
+}
+
+// NewCTBridgeSearch creates a instance of this stragegy
+func NewCTBridgeSearch() Learner {
+	return &BridgeSearch{common: newCommon()}
+}
+
+// Search searches for a network structure
+func (s *BridgeSearch) Search() Solution {
+	ct := model.SampleUniform(s.vs, s.tw)
+	s.paramLearner.Run(ct, s.ds.IntMaps())
+	return ct
+}
+
+// SetDataset sets dataset
+func (s *BridgeSearch) SetDataset(ds *data.Dataset) {
+	s.common.SetDataset(ds)
+	s.mutInfo = scr.ComputeMutInfDF(ds.DataFrame())
+}

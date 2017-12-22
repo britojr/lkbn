fe2af05407689e5e6de0914a033ed9f4c7c95a80
Joao Brito <joaojr.89@gmail.com>
2017-12-22 15:20:01 -0200

    learner: split lkm learners

--- learner/bridge.go
+++ learner/bridge.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/britojr/btbn/scr"
 	"github.com/britojr/lkbn/data"
-	"github.com/britojr/lkbn/emlearner"
-	"github.com/britojr/lkbn/factor"
 	"github.com/britojr/lkbn/model"
 	"github.com/britojr/lkbn/vars"
 )
@@ -22,69 +20,53 @@ func NewCTBridgeSearch() Learner {
 	return &BridgeSearch{common: newCommon()}
 }
 
-// Search searches for a network structure
-func (s *BridgeSearch) Search() Solution {
-	ct := model.SampleUniform(s.vs, s.tw)
-	// s.paramLearner.Run(ct, s.ds.IntMaps())
-
-	fmt.Println("Grouping variables per mutual information")
-	gs := splitGroups(s.ds.Variables(), s.tw, s.mutInfo)
-	for i, g := range gs {
-		fmt.Printf("%v: %v\n", i, g)
-	}
-	fmt.Println("Computing mutual information per groups")
-	gmi := groupMI(gs, s.mutInfo)
-	for i := range gmi {
-		fmt.Printf("%v\n", gmi[i])
-	}
-
-	fmt.Println("Finding clusters...")
-	gids := make([]int, len(gs))
-	for i := range gs {
-		gids[i] = i
-	}
-	cls := make([][]int, 0)
-	for len(gids) > 0 {
-		cluster := highestGroupPair(gids, gmi)
-		intsRemove(&gids, cluster[0])
-		intsRemove(&gids, cluster[1])
-		for {
-			if len(gids) == 0 {
-				break
-			}
-			x, _ := highestGroupToCluster(cluster, gids, gmi)
-			cluster = append(cluster, x)
-			intsRemove(&gids, x)
-			// TODO: add pseudo-lcm and pseudo-ltm2l functions here
-			if len(cluster) > 3 {
-				break
-			}
-		}
-		cls = append(cls, cluster)
-		if len(gids) == 1 {
-			cls = append(cls, []int{gids[0]})
-			intsRemove(&gids, gids[0])
-		}
-	}
-
-	for i := range cls {
-		fmt.Printf("%v: %v\n", i, cls[i])
-	}
-	return ct
-}
-
 // SetDataset sets dataset
 func (s *BridgeSearch) SetDataset(ds *data.Dataset) {
 	s.common.SetDataset(ds)
 	s.mutInfo = scr.ComputeMutInfDF(ds.DataFrame())
 }
 
-func splitGroups(vs vars.VarList, k int, mutInfo *scr.MutInfo) (gs []vars.VarList) {
+// Search searches for a network structure
+func (s *BridgeSearch) Search() Solution {
+	// TODO: use a copy of paramLearner with 'reuse' parm set to false
+	ct := model.SampleUniform(s.vs, s.tw)
+	// s.paramLearner.Run(ct, s.ds.IntMaps())
+
+	fmt.Println("Grouping variables per mutual information")
+	gs := splitVarGroups(s.ds.Variables(), s.tw, s.mutInfo)
+	for i, g := range gs {
+		fmt.Printf("%v: %v\n", i, g)
+	}
+	fmt.Println("Computing mutual information per groups")
+	gmi := computeGroupedMI(gs, s.mutInfo)
+	for i := range gmi {
+		fmt.Printf("%v\n", gmi[i])
+	}
+	fmt.Println("Grouping the groups of variables")
+	cls := clusterGroups(gs, gmi)
+	for i := range cls {
+		fmt.Printf("%v: %v\n", i, cls[i])
+	}
+
+	// TODO: remove
+	// learnLKM1L(, ds, paramLearner)
+	return ct
+}
+
+type group struct {
+	vars.VarList
+	id int
+}
+
+// splits varlist in groups of size k, grouping variables by highest MI
+func splitVarGroups(vs vars.VarList, k int, mutInfo *scr.MutInfo) (gs []group) {
 	remain := vs.Copy()
+	id := 0
 	for len(remain) != 0 {
 		g := highestPair(remain, mutInfo)
-		remain.Remove(g[0].ID())
-		remain.Remove(g[1].ID())
+		for _, v := range g {
+			remain.Remove(v.ID())
+		}
 		for i := 2; i < k; i++ {
 			if len(remain) == 0 {
 				break
@@ -97,13 +79,17 @@ func splitGroups(vs vars.VarList, k int, mutInfo *scr.MutInfo) (gs []vars.VarLis
 			g.Add(remain[0])
 			remain.Remove(remain[0].ID())
 		}
-		gs = append(gs, g)
+		gs = append(gs, group{g, id})
+		id++
 	}
 	return
 }
 
 // finds the highest scoring pair of variables
 func highestPair(vs vars.VarList, mutInfo *scr.MutInfo) vars.VarList {
+	if len(vs) < 2 {
+		panic("learner: not enough variables to compute highest MI pair")
+	}
 	maxMI := 0.0
 	var x, y *vars.Var
 	for i, v := range vs {
@@ -134,43 +120,71 @@ func highestToGroup(vs, ws vars.VarList, mutInfo *scr.MutInfo) (*vars.Var, float
 	return x, maxMI
 }
 
-// computes mi between groups of variables
-func groupMI(gs []vars.VarList, mutInfo *scr.MutInfo) [][]float64 {
+// computes mutual information between groups of variables
+func computeGroupedMI(gs []group, mutInfo *scr.MutInfo) [][]float64 {
 	mat := make([][]float64, len(gs))
 	for i := range mat {
 		mat[i] = make([]float64, len(gs))
 	}
-
 	for i := 0; i < len(gs); i++ {
 		for j := 0; j < i; j++ {
-			_, maxMI := highestToGroup(gs[i], gs[j], mutInfo)
+			_, maxMI := highestToGroup(gs[i].VarList, gs[j].VarList, mutInfo)
 			mat[i][j], mat[j][i] = maxMI, maxMI
 		}
 	}
 	return mat
 }
 
-func highestGroupPair(gids []int, gmi [][]float64) []int {
+func clusterGroups(gs []group, gmi [][]float64) [][]group {
+	fmt.Println("Finding clusters...")
+	cls := make([][]group, 0)
+	for len(gs) > 0 {
+		cl := highestGroupPair(gs, gmi)
+		for _, g := range cl {
+			groupRemove(&gs, g)
+		}
+		for {
+			if len(gs) == 0 {
+				break
+			}
+			g, _ := highestGroupToCluster(cl, gs, gmi)
+			cl = append(cl, g)
+			groupRemove(&gs, g)
+			// TODO: add pseudo-lcm and pseudo-ltm2l functions here
+			if len(cl) > 3 {
+				break
+			}
+		}
+		cls = append(cls, cl)
+		if len(gs) == 1 {
+			cls = append(cls, gs)
+			gs = nil
+		}
+	}
+	return cls
+}
+
+func highestGroupPair(gs []group, gmi [][]float64) []group {
 	maxMI := 0.0
-	var x, y int
-	for i, v := range gids {
-		for _, w := range gids[i+1:] {
-			mi := gmi[v][w]
+	var x, y group
+	for i, v := range gs {
+		for _, w := range gs[i+1:] {
+			mi := gmi[v.id][w.id]
 			if mi > maxMI {
 				maxMI = mi
 				x, y = v, w
 			}
 		}
 	}
-	return []int{x, y}
+	return []group{x, y}
 }
 
-func highestGroupToCluster(vs, ws []int, gmi [][]float64) (int, float64) {
+func highestGroupToCluster(vs, ws []group, gmi [][]float64) (group, float64) {
 	maxMI := 0.0
-	var x int
+	var x group
 	for _, v := range vs {
 		for _, w := range ws {
-			mi := gmi[v][w]
+			mi := gmi[v.id][w.id]
 			if mi > maxMI {
 				maxMI = mi
 				x = w
@@ -180,92 +194,11 @@ func highestGroupToCluster(vs, ws []int, gmi [][]float64) (int, float64) {
 	return x, maxMI
 }
 
-func intsRemove(xs *[]int, y int) {
-	for j, x := range *xs {
-		if x == y {
-			(*xs) = append((*xs)[:j], (*xs)[j+1:]...)
+func groupRemove(gs *[]group, g group) {
+	for j, h := range *gs {
+		if h.id == g.id {
+			(*gs) = append((*gs)[:j], (*gs)[j+1:]...)
 			break
 		}
 	}
-}
-
-func computeBIC(ct *model.CTree) float64 {
-	// TODO: check this bic computation
-	numparms := 0
-	for _, nd := range ct.Nodes() {
-		numparms += len(nd.Potential().Values())
-	}
-	return ct.Score() - float64(numparms)
-}
-
-func learnLKM1L(gs []vars.VarList, ds *data.Dataset, paramLearner emlearner.EMLearner) *model.CTree {
-	// create new latent variable
-	nstate := 2
-	lv := vars.New(len(ds.Variables()), nstate, "", true)
-	// mount structure
-	f := factor.New(gs[0].Union([]*vars.Var{lv})...)
-	ct := model.NewCTree()
-	root := model.NewCTNode()
-	root.SetPotential(f)
-	ct.AddNode(root)
-	for _, g := range gs[1:] {
-		f := factor.New(g.Union([]*vars.Var{lv})...)
-		nd := model.NewCTNode()
-		nd.SetPotential(f)
-		root.AddChildren(nd)
-		ct.AddNode(nd)
-	}
-
-	// increase latent cardinality and learn parameters until bic stops increasing
-	ct, _, _ = paramLearner.Run(ct, ds.IntMaps())
-	bic := computeBIC(ct)
-	for {
-		nstate++
-		lv.SetNState(nstate)
-		ct, _, _ = paramLearner.Run(ct, ds.IntMaps())
-		newbic := computeBIC(ct)
-		if newbic <= bic {
-			break
-		}
-		bic = newbic
-	}
-	return ct
-}
-
-func learnLKM2L(gs []vars.VarList, ds *data.Dataset, paramLearner emlearner.EMLearner) (*model.CTree, int) {
-	// TODO: implement correct 2L
-	// create new latent variable
-	nstate := 2
-	lvs := []*vars.Var{
-		vars.New(len(ds.Variables()), nstate, "", true),
-		vars.New(len(ds.Variables())+1, nstate, "", true),
-	}
-	// mount structure
-	f := factor.New(gs[0].Union([]*vars.Var{lvs[0]})...)
-	ct := model.NewCTree()
-	root := model.NewCTNode()
-	root.SetPotential(f)
-	ct.AddNode(root)
-	for _, g := range gs[1:] {
-		f := factor.New(g.Union([]*vars.Var{lvs[0]})...)
-		nd := model.NewCTNode()
-		nd.SetPotential(f)
-		root.AddChildren(nd)
-		ct.AddNode(nd)
-	}
-
-	// increase latent cardinality and learn parameters until bic stops increasing
-	ct, _, _ = paramLearner.Run(ct, ds.IntMaps())
-	bic := computeBIC(ct)
-	for {
-		nstate++
-		lvs[0].SetNState(nstate)
-		ct, _, _ = paramLearner.Run(ct, ds.IntMaps())
-		newbic := computeBIC(ct)
-		if newbic <= bic {
-			break
-		}
-		bic = newbic
-	}
-	return ct, len(lvs)
 }
---
+++ learner/lkm.go
@@ -0,0 +1,96 @@
+package learner
+
+import (
+	"github.com/britojr/lkbn/data"
+	"github.com/britojr/lkbn/emlearner"
+	"github.com/britojr/lkbn/factor"
+	"github.com/britojr/lkbn/model"
+	"github.com/britojr/lkbn/vars"
+)
+
+func computeBIC(ct *model.CTree) float64 {
+	// TODO: replace temporary approximation of BIC by correct equation
+	numparms := 0
+	for _, nd := range ct.Nodes() {
+		numparms += len(nd.Potential().Values())
+	}
+	return ct.Score() - float64(numparms)
+}
+
+// creates a new tree structure with lv as parent of every clique
+func createInitialStruct(gs []vars.VarList, lv *vars.Var) *model.CTree {
+	ct := model.NewCTree()
+	root := model.NewCTNode()
+	root.SetPotential(factor.New(gs[0].Union([]*vars.Var{lv})...))
+	ct.AddNode(root)
+	for _, g := range gs[1:] {
+		nd := model.NewCTNode()
+		nd.SetPotential(factor.New(g.Union([]*vars.Var{lv})...))
+		root.AddChildren(nd)
+		ct.AddNode(nd)
+	}
+	return ct
+}
+
+func learnLKM1L(gs []vars.VarList, ds *data.Dataset, paramLearner emlearner.EMLearner) *model.CTree {
+	// create new latent variable
+	nstate := 2
+	lv := vars.New(len(ds.Variables()), nstate, "", true)
+	ct := createInitialStruct(gs, lv)
+
+	// increase latent cardinality and learn parameters until bic stops increasing
+	ct, _, _ = paramLearner.Run(ct, ds.IntMaps())
+	bic := computeBIC(ct)
+	for {
+		nstate++
+		lv.SetNState(nstate)
+		for _, nd := range ct.Nodes() {
+			nd.Potential().ResetValues().RandomDistribute()
+		}
+		ct, _, _ = paramLearner.Run(ct, ds.IntMaps())
+		newbic := computeBIC(ct)
+		if newbic <= bic {
+			break
+		}
+		bic = newbic
+	}
+	return ct
+}
+
+func learnLKM2L(gs []vars.VarList, ds *data.Dataset, paramLearner emlearner.EMLearner) (*model.CTree, int) {
+	// TODO: implement correct 2L
+	// create new latent variable
+	nstate := 2
+	lvs := []*vars.Var{
+		vars.New(len(ds.Variables()), nstate, "", true),
+		vars.New(len(ds.Variables())+1, nstate, "", true),
+	}
+	// mount structure
+	f := factor.New(gs[0].Union([]*vars.Var{lvs[0]})...)
+	ct := model.NewCTree()
+	root := model.NewCTNode()
+	root.SetPotential(f)
+	ct.AddNode(root)
+	for _, g := range gs[1:] {
+		f := factor.New(g.Union([]*vars.Var{lvs[0]})...)
+		nd := model.NewCTNode()
+		nd.SetPotential(f)
+		root.AddChildren(nd)
+		ct.AddNode(nd)
+	}
+
+	// increase latent cardinality and learn parameters until bic stops increasing
+	ct, _, _ = paramLearner.Run(ct, ds.IntMaps())
+	bic := computeBIC(ct)
+	for {
+		nstate++
+		lvs[0].SetNState(nstate)
+		ct, _, _ = paramLearner.Run(ct, ds.IntMaps())
+		newbic := computeBIC(ct)
+		if newbic <= bic {
+			break
+		}
+		bic = newbic
+	}
+	return ct, len(lvs)
+}

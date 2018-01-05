d11d5c6b2f07c889ace31d6c2e66699f91db8498
Joao Brito <joaojr.89@gmail.com>
2018-01-05 14:49:02 -0200

    add graph package

---
+++ graph/graph.go
@@ -0,0 +1,13 @@
+package graph
+
+// WEdge defines a weighted edge
+type WEdge struct {
+	Head, Tail string
+	Weight     float64
+}
+
+// MaxSpanningTree receives a list of weighted edges of a full graph
+// and returns the list of weighted edges of a maximum spanning tree
+func MaxSpanningTree(full []WEdge) (mst []WEdge) {
+	return nil
+}
---
+++ graph/graph_test.go
@@ -0,0 +1,7 @@
+package graph
+
+import "testing"
+
+func TestMaxSpanningTree(t *testing.T) {
+
+}

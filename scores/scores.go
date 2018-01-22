53d7f355c5999d784b97e9d7a2eaf5c4817d490c
Joao Brito <joaojr.89@gmail.com>
2018-01-22 16:44:21 -0200

    scores: add compute loglikelihood

--- scores/scores.go
+++ scores/scores.go
@@ -1,13 +1,16 @@
 package scores
 
 import (
+	"log"
 	"math"
 
+	"github.com/britojr/lkbn/inference"
 	"github.com/britojr/lkbn/model"
 )
 
 // ComputeBIC computes Bayesian Information Criterion:
 // 	BIC = LL - ( model_size * ln(N)/2 )
+// WARNING: this method assumes that LL is computed and set
 func ComputeBIC(ct *model.CTree, intMaps []map[int]int) float64 {
 	modelsize := computeModelSize(ct)
 	return ct.Score() - float64(modelsize)*math.Log(float64(len(intMaps)))/2.0
@@ -23,4 +26,18 @@ func computeModelSize(ct *model.CTree) (modelsize int) {
 		}
 	}
 	return
+}
+
+// ComputeLL computes the Log Likelihood of the given model
+func ComputeLL(ct *model.CTree, intMaps []map[int]int) float64 {
+	infalg := inference.NewCTreeCalibration(ct)
+	var ll float64
+	for _, evid := range intMaps {
+		evLkhood := infalg.Run(evid)
+		if evLkhood == 0 {
+			log.Panicf("scores: invalid prob of evidence == 0")
+		}
+		ll += math.Log(evLkhood)
+	}
+	return ll
 }

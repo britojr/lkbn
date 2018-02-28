cab415b75ffdbbec5a45dc9709c393a5319ba171
Joao Brito <joaojr.89@gmail.com>
2018-02-28 16:21:48 -0300

    cmd: complete kl-divergence

--- cmd/kldiv/kldiv.go
+++ cmd/kldiv/kldiv.go
@@ -5,10 +5,14 @@ import (
 	"log"
 	"os"
 	"time"
+
+	"github.com/britojr/lkbn/inference"
+	"github.com/britojr/lkbn/model"
+	"gonum.org/v1/gonum/floats"
 )
 
 func main() {
-	var bnFile, ctFile string
+	var orgFile, compFile string
 
 	if len(os.Args) < 3 {
 		fmt.Printf("\n error: missing network files\n\n")
@@ -16,15 +20,31 @@ func main() {
 		fmt.Printf("\tkldiv <original-network> <comparision-network>\n\n")
 		os.Exit(1)
 	}
-	bnFile, ctFile = os.Args[1], os.Args[2]
+	orgFile, compFile = os.Args[1], os.Args[2]
 	log.Printf("========== COMPARING NETWORKS =====================\n")
-	log.Printf("Comparing %v || %v\n", bnFile, ctFile)
+	log.Printf("Comparing %v || %v\n", orgFile, compFile)
 
 	start := time.Now()
-	kld := 0.0
+	orgNet := model.ReadBNetXML(orgFile)
+	compNet := model.ReadCTree(compFile)
+	kld := KLDiv(orgNet, compNet)
 	elapsed := time.Since(start)
 
 	log.Printf("Time: %v\n", elapsed)
 	log.Printf("KL-divergence: %.6f\n", kld)
 	log.Printf("---------------------------------------------------\n")
+}
+
+// KLDiv computes kl-divergence
+func KLDiv(orgNet *model.BNet, compNet *model.CTree) (kld float64) {
+	infalg := inference.NewCTreeCalibration(compNet)
+	for _, v := range orgNet.Variables() {
+		pcond := orgNet.Node(v).Potential().Copy()
+		family := pcond.Variables()
+		qjoint := infalg.Posterior(family, nil)
+		pjoint := orgNet.MarginalizedFamily(v)
+
+		kld += floats.Sum(pjoint.Times(pcond.Log().Minus(qjoint.Log())).Values())
+	}
+	return kld
 }

33a66a321b3f110b92d7c84d6b8dc37e009fc758
Joao Brito <joaojr.89@gmail.com>
2018-03-06 20:21:13 -0300

    .

--- learner/biext.go
+++ learner/biext.go
@@ -54,38 +54,12 @@ func (s *BIextSearch) SetFileParameters(props map[string]string) {
 
 // Search searches for a network structure
 func (s *BIextSearch) Search() Solution {
-	queue := []*vars.Var{findRoot(s.bn)}
-	bn := model.NewBNet()
-	vs := s.bn.Variables()
-	for len(queue) > 0 {
-		v := queue[0]
-		nd := model.NewBNode(v)
-		nd.SetPotential(s.bn.Node(v).Potential())
-		bn.AddNode(nd)
-		chs := findChildren(s.bn, v)
-		for _, v := range chs {
-			if v.Latent() {
-				queue = append(queue, v)
-				chs.Remove(v.ID())
-			}
-		}
-		fmt.Println(chs)
-		s.initOptimizer() // TODO: create a new optimizer because unwanted colateral from timeout
-		s.iterAlg.(*optimizer.IterativeSearch).SetVarsSubSet(listIDs(chs))
-		bnStruct := optimizer.Search(s.iterAlg, s.maxIterCl, s.maxTimeCl)
-		for _, u := range chs {
-			nd := model.NewBNode(u)
-			parents := bnStruct.Parents(u.ID()).DumpAsInts()
-			scope := vars.VarList{u}
-			scope.Add(v)
-			for _, id := range parents {
-				scope.Add(vs.FindByID(id))
-			}
-			nd.SetPotential(factor.New(scope...))
-			bn.AddNode(nd)
-		}
-		queue = queue[1:]
-	}
+	bn, _ := s.buildExtStructures()
+	// TODO:
+	// - learn parameters for clique tree
+	// - use the learned ctree to set bnet parameters
+	// - compute scores
+	//
 	// log.Printf("--------------------------------------------------\n")
 	// log.Printf("LL: %.6f\n", bn.Score())
 	return bn
@@ -122,4 +96,75 @@ func listIDs(vs []*vars.Var) []int {
 		is = append(is, v.ID())
 	}
 	return is
+}
+
+func (s *BIextSearch) buildExtStructures() (*model.BNet, *model.CTree) {
+	// TODO:
+	// - simutaneously create the cliquetree structure
+	queue := []*vars.Var{findRoot(s.bn)}
+	bn := model.NewBNet()
+	ct := model.NewCTree()
+	vs := s.bn.Variables()
+	for len(queue) > 0 {
+		v := queue[0]
+		nd := model.NewBNode(v)
+		nd.SetPotential(s.bn.Node(v).Potential().Copy())
+		bn.AddNode(nd)
+		ctnd := model.NewCTNode()
+		ctnd.SetPotential(s.bn.Node(v).Potential().Copy())
+		pa := ct.FindNodeContaining(ctnd.Variables().Diff(vars.VarList{v}))
+		if pa != nil {
+			pa.AddChild(ctnd)
+		}
+		ct.AddNode(ctnd)
+		chs := findChildren(s.bn, v)
+		for _, v := range chs {
+			if v.Latent() {
+				queue = append(queue, v)
+				chs.Remove(v.ID())
+			}
+		}
+		fmt.Println(chs)
+		// TODO: will need the used order to build the clique tree
+		s.initOptimizer() // TODO: create a new optimizer because unwanted colateral from timeout
+		s.iterAlg.(*optimizer.IterativeSearch).SetVarsSubSet(listIDs(chs))
+		bnStruct := optimizer.Search(s.iterAlg, s.maxIterCl, s.maxTimeCl)
+
+		ctSubRoot := model.NewCTNode()
+		ctSubRoot.SetPotential(factor.New())
+		ctnds := []*model.CTNode{}
+		invs := []*vars.Var{}
+		for _, u := range chs {
+			nd := model.NewBNode(u)
+			parents := bnStruct.Parents(u.ID()).DumpAsInts()
+			scope := vars.VarList{u}
+			scope.Add(v)
+			for _, id := range parents {
+				scope.Add(vs.FindByID(id))
+			}
+			nd.SetPotential(factor.New(scope...))
+			bn.AddNode(nd)
+			if len(nd.Potential().Variables()) <= s.Treewidth() {
+				ctSubRoot.Potential().Times(nd.Potential())
+			} else {
+				ctnd := model.NewCTNode()
+				ctnd.SetPotential(nd.Potential().Copy())
+				ctnds = append(ctnds, ctnd)
+				invs = append(invs, u)
+			}
+		}
+
+		ctnd.AddChild(ctSubRoot)
+		ct.AddNode(ctSubRoot)
+		for i, ctnd := range ctnds {
+			pa := ct.FindNodeContaining(ctnd.Variables().Diff(vars.VarList{invs[i]}))
+			if pa != nil {
+				pa.AddChild(ctnd)
+			}
+			ct.AddNode(ctnd)
+		}
+
+		queue = queue[1:]
+	}
+	return bn, ct
 }
